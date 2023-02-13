15320fc7718f4fa46db92a5a0c599261abba1f75
unknown <eodrmfdl1004@naver.com>
2023-02-13 18:30:41 +0900

    Style: [events-app] 필터 ui 추가

--- events-app/pages/events/index.js
+++ events-app/pages/events/index.js
@@ -1,12 +1,15 @@
-import EventList from "@/components/events/EventList/EventList";
+import { Fragment } from "react";
 import { getAllEvents } from "@/dummy-data";
+import EventList from "@/components/events/EventList/EventList";
+import EventsSearch from "@/components/events/EventsSearch/EventsSearch";
 
 export default function AllEventsPage() {
   const events = getAllEvents();
 
   return (
-    <div>
+    <Fragment>
+      <EventsSearch />
       <EventList items={events} />
-    </div>
+    </Fragment>
   );
 }
