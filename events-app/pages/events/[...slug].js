e329598fe2b7022f88d275b71711ad88154f2f8f
unknown <eodrmfdl1004@naver.com>
2023-02-13 19:16:20 +0900

    Feat: [events-app] 조건 검색 페이지 - uri에 따른 UI

--- events-app/pages/events/[...slug].js
+++ events-app/pages/events/[...slug].js
@@ -1,4 +1,36 @@
+import { getFilteredEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
+
 export default function FilteredEventsPage() {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const [filteredYear, filteredMonth] = filterData;
+  const year = +filteredYear;
+  const month = +filteredMonth;
+
+  if (
+    isNaN(year) ||
+    isNaN(month) ||
+    year > 2030 ||
+    year < 2021 ||
+    month < 1 ||
+    month > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({ year, month });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>;
+  }
+
   return (
     <div>
       <h1>Filtered Events Page</h1>
