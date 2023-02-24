471c68707ca0c65e555441be9e9fa82a4ef40cd1
unknown <eodrmfdl1004@naver.com>
2023-02-24 12:44:34 +0900

    Add: [events-app] 댓글, 뉴스레터 컴포넌트 추가

--- events-app/pages/index.js
+++ events-app/pages/index.js
@@ -1,5 +1,7 @@
 import { getFeaturedEvents } from "@/helpers/api-util";
 import EventList from "@/components/events/EventList/EventList";
+import Head from "next/head";
+import NewsletterRegistration from "@/components/input/NewsletterRegistration/NewsletterRegistration";
 
 export default function HomePage({ featuredEvents }) {
   if (!featuredEvents) {
@@ -8,6 +10,15 @@ export default function HomePage({ featuredEvents }) {
 
   return (
     <div>
+      <Head>
+        <title> Events App</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
+      </Head>
+
+      <NewsletterRegistration />
       <EventList items={featuredEvents} />
     </div>
   );
