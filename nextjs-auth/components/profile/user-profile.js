f593b03231b76bed4c41ac5b7ce4ecca6e2dfa60
unknown <eodrmfdl1004@naver.com>
2023-03-09 20:32:32 +0900

    페이지 가드

--- nextjs-auth/components/profile/user-profile.js
+++ nextjs-auth/components/profile/user-profile.js
@@ -1,8 +1,26 @@
-import ProfileForm from './profile-form';
-import classes from './user-profile.module.css';
+import { useEffect, useState } from "react";
+import classes from "./user-profile.module.css";
+
+import { getSession, useSession } from "next-auth/react";
+import ProfileForm from "./profile-form";
 
 function UserProfile() {
-  // Redirect away if NOT auth
+  const [isLoading, setIsLoading] = useState(true);
+  // const { data: session, status } = useSession();
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session) {
+        window.location.href = "/auth";
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, []);
+
+  if (isLoading) {
+    return <p className={classes.profile}>Loading...</p>;
+  }
 
   return (
     <section className={classes.profile}>
