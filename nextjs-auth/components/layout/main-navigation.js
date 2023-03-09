e520c68eda619e98b350ac5048a0d0046f55aeff
unknown <eodrmfdl1004@naver.com>
2023-03-09 19:55:38 +0900

    활성 세션 관리

--- nextjs-auth/components/layout/main-navigation.js
+++ nextjs-auth/components/layout/main-navigation.js
@@ -1,8 +1,12 @@
 import Link from "next/link";
-
 import classes from "./main-navigation.module.css";
 
+import { useSession } from "next-auth/react";
+
 function MainNavigation() {
+  const { data: session, status } = useSession();
+  const loading = status === "loading";
+
   return (
     <header className={classes.header}>
       <Link href='/'>
@@ -10,15 +14,23 @@ function MainNavigation() {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link href='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link href='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!session && !loading && (
+            <li>
+              <Link href='/auth'>Login</Link>
+            </li>
+          )}
+
+          {session && (
+            <li>
+              <Link href='/profile'>Profile</Link>
+            </li>
+          )}
+
+          {session && (
+            <li>
+              <button>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
--- nextjs-auth/pages/_app.js
+++ nextjs-auth/pages/_app.js
@@ -1,11 +1,15 @@
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import Layout from "../components/layout/layout";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+import { SessionProvider } from "next-auth/react";
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   );
 }
 
